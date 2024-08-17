d439c1daf56ee6fb31c1a9b5c442f8e425860b27
sohamgade12 <sohamgade101@gmail.com>
2024-08-17 16:54:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log("Hello DreamsGuider to console");
+
+//premitive format => number , String, Boolean, Symbol, NULL, Undefined
+//Referenence format=> Array[] Object{} Function() => 8 types 
+
+//var let const
+// ES5 ES6
+//ES5 => var 
+// ES6 => let const
+
+
+// var name="Vikram";
+// console.log(name);
+
+// var MobileNo=9373346490;
+// console.log(MobileNo);
+
+
+// var lastname;
+// console.log(lastname)
+
+
+
+    
+let a=[0,1,2,3,4,5,6,7,8];
+
+
+let b=a;
+b.pop();
+console.log(b);
+console.log(a);
+
+let c=5;
+let d=c;
+d=d-1;
+console.log(d);
+console.log(c);
+
