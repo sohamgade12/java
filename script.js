940d482fd05af9e3833c4ede7633ad620adff75b
sohamgade12 <sohamgade101@gmail.com>
2024-08-17 16:55:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,18 @@
-console.log("Hello DreamsGuider to console");
+function login(){
+    var username=document.getElementsByClassName("name").value;
+    // console.log("username is verified");
 
-//premitive format => number , String, Boolean, Symbol, NULL, Undefined
-//Referenence format=> Array[] Object{} Function() => 8 types 
-
-//var let const
-// ES5 ES6
-//ES5 => var 
-// ES6 => let const
-
-
-// var name="Vikram";
-// console.log(name);
-
-// var MobileNo=9373346490;
-// console.log(MobileNo);
-
-
-// var lastname;
-// console.log(lastname)
-
-
+    var password=document.getElementById("password").value;
+    // console.log("password is verified");
+    if(!username || !password){
+        console.log("Please enter valid username and password");
+    }
+    else if(username=="Vikram" && password=="Destiny@21")
+    {
+        console.log("username and password are verified");
+    }
 
     
-let a=[0,1,2,3,4,5,6,7,8];
+}
 
-
-let b=a;
-b.pop();
-console.log(b);
-console.log(a);
-
-let c=5;
-let d=c;
-d=d-1;
-console.log(d);
-console.log(c);
 
